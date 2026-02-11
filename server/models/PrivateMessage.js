1aa3727fb9694316618c7175a3e1630311417647
karina-vetlugina <karinavetlugina05@gmail.com>
2026-02-11 14:24:07 -0500

    add MongoDB models for users and messages

---
+++ server/models/PrivateMessage.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const privateMessageSchema = new mongoose.Schema({
+  from_user: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  to_user: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  message: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  date_sent: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("PrivateMessage", privateMessageSchema);
---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  firstname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdon: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
